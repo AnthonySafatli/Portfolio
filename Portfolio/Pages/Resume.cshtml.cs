857b408da8c8fdba032d821d01c85f7cf16bc6f0
Anthony <anthonydaniel123@live.com>
2024-03-26 00:52:06 -0300

    Added all planned pages

---
+++ Portfolio/Pages/Resume.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Portfolio.Pages
+{
+    public class ResumeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
