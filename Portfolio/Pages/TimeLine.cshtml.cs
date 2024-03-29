fe0815cfd62c558f0864e2fdc79e0268d3f34d66
Anthony <anthonydaniel123@live.com>
2024-03-28 23:19:58 -0300

    Started working on timeline page

--- Portfolio/Pages/TimeLine.cshtml.cs
+++ Portfolio/Pages/TimeLine.cshtml.cs
@@ -5,8 +5,15 @@ namespace Portfolio.Pages
 {
     public class TimeLineModel : PageModel
     {
+        public int WeeksLived { get; set; }
+
         public void OnGet()
         {
+            DateTime birthday = new DateTime(2004, 10, 14);
+            DateTime currentDate = DateTime.Today;
+
+            TimeSpan timeSpan = currentDate - birthday;
+            WeeksLived = (int)(timeSpan.TotalDays / 7);
         }
     }
 }
