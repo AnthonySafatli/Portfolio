410642fc548c4908df78ada6ed01c0ff34cf4097
Anthony <anthonydaniel123@live.com>
2024-03-29 00:26:36 -0300

    Added more

--- Portfolio/Pages/TimeLine.cshtml.cs
+++ Portfolio/Pages/TimeLine.cshtml.cs
@@ -1,19 +1,41 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Reflection;
 
 namespace Portfolio.Pages
 {
     public class TimeLineModel : PageModel
     {
         public int WeeksLived { get; set; }
+        public int Weeks { get; set; } = 4693;
+        public string[]? ClassList { get; set; }
 
         public void OnGet()
         {
+            ClassList = new string[Weeks];
+
             DateTime birthday = new DateTime(2004, 10, 14);
             DateTime currentDate = DateTime.Today;
 
             TimeSpan timeSpan = currentDate - birthday;
             WeeksLived = (int)(timeSpan.TotalDays / 7);
+
+            for (int i = 0; i < Weeks; i++)
+            {
+                ClassList[i] = "grid-item";
+                if (i > WeeksLived)
+                {
+                    ClassList[i] += " future-week";
+                }
+                //if (i > 10 && i < 50)
+                //{
+                //    ClassList[i] += " elem";
+                //}
+                // more if's
+            }
+
+            // ClassList[10] += " birthday";
+            // TODO: Add javascript for this
         }
     }
 }
