17e3876388a3df0ff7b344819a44296eaaac64a8
Anthony <anthonydaniel123@live.com>
2024-04-06 00:17:57 -0300

    Added projects page

--- Portfolio/Pages/Project.cshtml.cs
+++ Portfolio/Pages/Project.cshtml.cs
@@ -1,12 +1,40 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Portfolio.Data;
+using Portfolio.Models;
 
-namespace Portfolio.Pages
+namespace Portfolio.Pages;
+
+public class ProjectModel : PageModel
 {
-    public class ProjectModel : PageModel
+    private readonly ProjectsContext _context;
+
+    [BindProperty(SupportsGet=true)]
+    public string Name { get; set; }
+    public Project? Project { get; set; }
+
+    public ProjectModel(ProjectsContext context)
     {
-        public void OnGet()
+        _context = context;
+    }
+
+    public async Task<IActionResult> OnGetAsync()
+    {
+        Project = await _context.Projects.FirstOrDefaultAsync(x => x.Name == Name);
+
+        if (Project == null)
         {
+            Project = new Project()
+            {
+                Name = Name,
+                DateStarted = DateTime.MinValue,
+                DateEnded = DateTime.Now,
+            };
+
+            return NotFound(); // remove for testing
         }
+
+        return Page();
     }
 }
