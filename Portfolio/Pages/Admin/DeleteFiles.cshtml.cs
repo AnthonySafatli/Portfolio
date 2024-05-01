a1bc202c314723288faa1e85e276da9c40e8d432
Anthony <anthonydaniel123@live.com>
2024-05-01 12:55:13 -0300

    Added deleting files

--- Portfolio/Pages/Admin/DeleteFiles.cshtml.cs
+++ Portfolio/Pages/Admin/DeleteFiles.cshtml.cs
@@ -1,19 +1,95 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 using Portfolio.Data;
+using Portfolio.Models;
 
 namespace Portfolio.Pages.Admin;
 
 [Authorize]
 public class DeleteFilesModel : PageModel
 {
-    public void OnGet()
+    private readonly ProjectsContext _context;
+    private readonly IWebHostEnvironment _environment;
+
+    public List<string> Files { get; set; }
+
+    public DeleteFilesModel(ProjectsContext context, IWebHostEnvironment environment)
     {
-        // Get all files in 'projects' folder
-        // see if they are used in projects db
-        // display all items with a delete button
-        // post
-        // delete file
+        _context = context;
+        _environment = environment;
+    }
+
+    public async Task<IActionResult> OnGetAsync(string? file)
+    {
+        if (file != null)
+        {
+            string webRoot = _environment.WebRootPath;
+            string fullPath = Path.Combine(webRoot, "projects") + file;
+            try
+            {
+                System.IO.File.Delete(fullPath);
+            } catch (Exception ex) { }
+        }
+
+        Files = GetAllFiles();
+        IList<Project> projects = await _context.Projects.ToListAsync();
+
+        foreach (Project proj in projects)
+        {
+            string jsonPath = @"Projects\Json\" + proj.File + ".json";
+            if (!System.IO.File.Exists(jsonPath))
+                continue;
+
+            string jsonString = System.IO.File.ReadAllText(jsonPath);
+            if (jsonString == null)
+                continue;
+
+            ProjectPage projectJson = JsonConvert.DeserializeObject<ProjectPage>(jsonString);
+            if (projectJson == null)
+                continue;
+
+            foreach (PageElement elem in projectJson.Elements)
+            {
+                if (elem.Name == "media" && elem.Link != null)
+                {
+                    if (Files.Contains(elem.Link))
+                    {
+                        Files.Remove(elem.Link);
+                    }
+                }
+            }
+        }
+
+        return Page();
+    }
+
+    private List<string> GetAllFiles()
+    {
+        string dir = Path.Combine(_environment.WebRootPath, "projects");
+        List<string> files = GetAllFiles(dir);
+
+        string rootDir = Path.Combine(_environment.WebRootPath, "projects");
+        for (int i = 0; i < files.Count; i++)
+        {
+            files[i] = files[i].Remove(0, rootDir.Length);
+        }
+
+        return files;
+    }
+
+    private List<string> GetAllFiles(string rootDirectory)
+    {
+        List<string> files = new List<string>();
+
+        files.AddRange(Directory.GetFiles(rootDirectory));
+        foreach (string directory in Directory.GetDirectories(rootDirectory))
+        {
+            files.AddRange(GetAllFiles(directory));
+        }
+
+        return files;
     }
 }
