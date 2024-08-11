7e454323cc303549c1225b7fbc5a75a7b462ed24
Anthony <anthonydaniel123@live.com>
2024-08-11 17:34:26 -0300

    Removed fun section

--- Portfolio/Pages/Page/TimeLine.cshtml.cs
+++ Portfolio/Pages/Page/TimeLine.cshtml.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Reflection;
 
-namespace Portfolio.Pages.Fun;
+namespace Portfolio.Pages.Page;
 
 public class TimeLineModel : PageModel
 {
