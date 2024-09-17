e3986edd21e55d53222f0067ce99238f0fa8a292
Anthony <anthonydaniel123@live.com>
2024-09-17 10:38:32 -0300

    Moved python stuff to different project

--- Markdown to Json/md_to_json.py
+++ Markdown to Json/md_to_json.py
@@ -3,21 +3,24 @@ import enums_to_json
 import json
 import sys
 
-file_name = sys.argv[1]
+file_name = input("File Name: ")
 
 md_file = "Projects/Markdown/" + file_name + ".md"
 
-
+print("Opening File...")
 lines_list = []
 with open(md_file, 'r') as file:
     for line in file:
         lines_list.append(line.strip())
 
+print("Converting to enums...")
 data = []
 for line in lines_list:
     enum = md_to_enums.get_enum(line)
     data.append((line, enum))
  
+print("Converting to JSON...")
 dictionary = enums_to_json.get_dict(data)
 
+print("Saving to file...")
 enums_to_json.save_dict(dictionary, "Projects/Json/" + file_name + ".json")
--- Portfolio/run_python.ps1
+++
@@ -1,3 +0,0 @@
-.\/.venv/Scripts/Activate.ps1
-
-cd Portfolio/Scripts/
