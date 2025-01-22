a9ffa8c013d3fff40c045b5e0dcf038ae736be19
Anthony <anthonydaniel123@live.com>
2025-01-21 20:32:35 -0400

    Removed the time part of the date

--- Portfolio/Models/Project.cs
+++ Portfolio/Models/Project.cs
@@ -1,16 +1,24 @@
-﻿using System.Text;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
 
 namespace Portfolio.Models;
 
 public class Project
 {
     public string Name { get; set; }
+
+    [DataType(DataType.Date)]
     public DateTime DateStarted { get; set; }
+    [DataType(DataType.Date)]
     public DateTime DateEnded { get; set; }     
+    
     public string? PageContent {  get; set; }
     public string Thumbnail { get; set; }
-    public bool Hidden { get; set; }
     public string Description { get; set; }
+    
+    public bool Hidden { get; set; }
+    
     public string? Github { get; set; }
     public string? Page { get; set; }
     public string? Download { get; set; }
