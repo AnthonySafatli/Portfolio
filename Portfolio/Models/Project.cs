f1f4ac432edf1d3915da4e364cd4bc4651b5da66
Anthony <anthonydaniel123@live.com>
2025-01-24 16:43:25 -0400

    Improvements to the upload system

--- Portfolio/Models/Project.cs
+++ Portfolio/Models/Project.cs
@@ -7,18 +7,23 @@ namespace Portfolio.Models;
 public class Project
 {
     public string Id { get; set; }
+    [Display(Name = "Display Name")]
     public string DisplayName { get; set; }
 
+    [Display(Name = "Date Started")]
     [DataType(DataType.Date)]
     public DateTime DateStarted { get; set; }
+    [Display(Name = "Date Ended")]
     [DataType(DataType.Date)]
-    public DateTime DateEnded { get; set; }     
-    
+    public DateTime DateEnded { get; set; }
+
+    [Display(Name = "Page Content")]
     public string? PageContent {  get; set; }
     public string Thumbnail { get; set; }
     public string Description { get; set; }
     
     public bool Hidden { get; set; }
+    [Display(Name = "Sort Order")]
     public int SortOrder { get; set; }
     
     public string? Github { get; set; }
